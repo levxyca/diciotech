fe0c40f2c07e1e9592efa4fb1d8a8e60ac95c9c7
armindomatheus <matheusab_gv@hotmail.com>
2023-09-28 03:23:50 -0300

    feat: filtro; Implementado filtragem e polição nas palavras de campos semanticos, e ajustado ao sass

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,44 @@ import "./dark_mode.js";
 
 const searchInput = document.querySelector("#search-input");
 const cardsSection = document.querySelector("#cards");
+const filterSelect = document.querySelector("#filter_select");
+let listOfCardsFiltered = [];
 
 function searchCards() {
     const inputValue = searchInput.value.toLowerCase();
-    const listOfCards = document.querySelectorAll(".card");
-
-    for (const card of listOfCards) {
+    for (const card of listOfCardsFiltered) {
         const cardContent = card.textContent.toLowerCase();
         card.style.display = cardContent.includes(inputValue) ? "" : "none";
     }
 }
 
+function filter() {
+    listOfCardsFiltered = [];
+    const listOfCards = document.querySelectorAll(".card");
+    const listOfTags = document.querySelectorAll(".card_tags");
+    for (let i = 0; i < listOfCards.length; i++) {
+        if (listOfTags[i].textContent.includes(filterSelect.value) || filterSelect.value == "") {
+            listOfCards[i].style.display = "";
+            listOfCardsFiltered.push(listOfCards[i])
+        }
+        else {
+            listOfCards[i].style.display = "none"
+        }
+    }
+    searchCards()
+}
+
 function insertCardsIntoHtml(data) {
     let cards = "";
     data.forEach((card) => {
+        if (!card.tags) {
+            card.tags = "";
+        }
         cards += `
         <section class="card">
             <h3 class="card__title">${card.title}</h3>
             <p class="card__description">${card.description}</p>
+            <p class="card_tags" style = "display: none;">${card.tags}</p>
         `;
         if (card.content && card.content.code) {
             cards += `
@@ -31,6 +51,21 @@ function insertCardsIntoHtml(data) {
         cards += "</section>";
     });
     cardsSection.innerHTML = cards;
+    filter();
+}
+
+function getTagsFromCards(data) {
+    const tags = [];
+    data.forEach((card) => {
+        if (card.tags) {
+            card.tags.forEach((tag) => {
+                if (!tags.includes(tag)) {
+                    tags.push(tag);
+                }
+            });
+        }
+    });
+    insertTagsInSelect(tags);
 }
 
 async function sortCardsByTitle(data) {
@@ -43,10 +78,25 @@ async function getCardsFromJson() {
         const data = await res.json();
         const sortedCards = await sortCardsByTitle(data);
         insertCardsIntoHtml(sortedCards);
+        getTagsFromCards(sortedCards);
     } catch (error) {
         console.error("An error occurred while fetching card data.", error);
     }
 }
 
+function insertTagsInSelect(tags) {
+    let option = document.createElement("option");
+    option.value = "";
+    option.text = "Todos";
+    filterSelect.appendChild(option)
+    for (const tag of tags) {
+        option = document.createElement("option");
+        option.value = tag;
+        option.text = tag;
+        filterSelect.appendChild(option)
+    }
+}
+
 searchInput.addEventListener("input", searchCards);
+filterSelect.addEventListener("change", filter)
 getCardsFromJson();
