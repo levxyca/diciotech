2a1de3e92e500ce76a71a94d590869da528ef666
chrishenderson07 <chris.hend012@gmail.com>
2023-09-29 13:28:41 -0300

    insert optional chaining Term

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,51 @@
-import "./check_color_mode.js"
 import "./dark_mode.js";
 
 const searchInput = document.querySelector("#search-input");
 const cardsSection = document.querySelector("#cards");
 
 function searchCards() {
-    const inputValue = searchInput.value.toLowerCase();
-    const listOfCards = document.querySelectorAll(".card");
+	const inputValue = searchInput.value.toLowerCase();
+	const listOfCards = document.querySelectorAll(".card");
 
-    for (const card of listOfCards) {
-        const cardContent = card.textContent.toLowerCase();
-        card.style.display = cardContent.includes(inputValue) ? "" : "none";
-    }
+	for (const card of listOfCards) {
+		const cardContent = card.textContent.toLowerCase();
+		card.style.display = cardContent.includes(inputValue) ? "" : "none";
+	}
 }
 
 function insertCardsIntoHtml(data) {
-    let cards = "";
-    data.forEach((card) => {
-        cards += `
+	let cards = "";
+	data.forEach((card) => {
+		cards += `
         <section class="card">
             <h3 class="card__title">${card.title}</h3>
             <p class="card__description">${card.description}</p>
         `;
-        if (card.content && card.content.code) {
-            cards += `
+		if (card.content && card.content.code) {
+			cards += `
             <div class="card__content">
                 <code class="card__code">${card.content.code}</code>
             </div>
             `;
-        }
-        cards += "</section>";
-    });
-    cardsSection.innerHTML = cards;
+		}
+		cards += "</section>";
+	});
+	cardsSection.innerHTML = cards;
 }
 
 async function sortCardsByTitle(data) {
-    return data.cards.sort((a, b) => a.title.localeCompare(b.title));
+	return data.cards.sort((a, b) => a.title.localeCompare(b.title));
 }
 
 async function getCardsFromJson() {
-    try {
-        const res = await fetch("./assets/data/cards_pt-br.json");
-        const data = await res.json();
-        const sortedCards = await sortCardsByTitle(data);
-        insertCardsIntoHtml(sortedCards);
-    } catch (error) {
-        console.error("An error occurred while fetching card data.", error);
-    }
+	try {
+		const res = await fetch("./assets/data/cards_pt-br.json");
+		const data = await res.json();
+		const sortedCards = await sortCardsByTitle(data);
+		insertCardsIntoHtml(sortedCards);
+	} catch (error) {
+		console.error("An error occurred while fetching card data.", error);
+	}
 }
 
 searchInput.addEventListener("input", searchCards);
