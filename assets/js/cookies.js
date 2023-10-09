7d6fedea7981930cb50b763a74d17ceb51af595a
thiagofqs <39809188+thiagofqs@users.noreply.github.com>
2023-10-09 14:19:54 -0300

    refactor: separado códigos de cookies em arquivos separados e o css para sass

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -2,10 +2,10 @@ function loadCookies() {
     const cookiesAccepted = localStorage.getItem("cookiesAccepted");
     const acceptBtn = document.querySelector(".cookies-accept-button");
     const container = document.querySelector(".cookies-container");
-    
+
     if (!cookiesAccepted) {
         container.style.display = "block";
-        
+
         acceptBtn.addEventListener("click", () => {
             localStorage.setItem("cookiesAccepted", true);
             container.style.display = "none";
