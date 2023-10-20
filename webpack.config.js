de600c5c5a84233f92af0c2b5bb4f14c84d6bb1a
rwietter <rwietter@zohomail.com>
2023-10-19 23:19:00 -0300

    chore: add webpack to build bundle CLI

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './cli/index.ts',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'cli', 'build'),
+  },
+  mode: 'production',
+};
