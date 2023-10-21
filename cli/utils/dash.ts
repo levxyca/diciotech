b84c92294a49d02ca5175c798dc4c234ab0074df
rwietter <rwietter@zohomail.com>
2023-10-20 23:39:23 -0300

    refactor(cli/utils): add dash prompt styles

---
+++ cli/utils/dash.ts
@@ -0,0 +1,7 @@
+/**
+ * Repeats a character a given amount of times.
+ * @param {string} c - The character to be repeated.
+ * @param {number} qt - The amount of times the character should be repeated.
+ * @returns {string} The repeated character.
+ */
+export const dash = (c: string) => (qt: number) => c.repeat(qt);
