4c7174215183d10cade44db0d1af87ef1ccc7e9e
George Araújo <george.gcac@gmail.com>
2024-10-16 16:43:51 -0300

    Fixed blinking when loading dark theme, added system theme
    Signed-off-by: George Araújo <george.gcac@gmail.com>

---
+++ assets/js/theme.js
@@ -0,0 +1,70 @@
+// has to be in the head tag, otherwise a flicker effect will occur
+const body = document.body;
+const changeThemeButton = document.querySelector("#change-theme-button");
+const moonIcon = document.querySelector(".ph-moon");
+const sunIcon = document.querySelector(".ph-sun");
+const systemIcon = document.querySelector(".ph-desktop");
+
+let toggleThemeSetting = () => {
+    let themeSetting = getThemeSetting();
+    if (themeSetting == "system") {
+        setThemeSetting("light");
+    } else if (themeSetting == "light") {
+        setThemeSetting("dark");
+    } else {
+        setThemeSetting("system");
+    }
+};
+
+let getThemeSetting = () => {
+    let themeSetting = localStorage.getItem("theme");
+    if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
+        themeSetting = "system";
+    }
+    return themeSetting;
+};
+
+let setThemeSetting = (themeSetting) => {
+    localStorage.setItem("theme", themeSetting);
+    document.documentElement.setAttribute("data-theme-setting", themeSetting);
+    applyTheme();
+};
+
+let applyTheme = () => {
+    let theme = getTheme();
+    document.documentElement.setAttribute("data-theme", theme);
+};
+
+let getTheme = () => {
+    let themeSetting = getThemeSetting();
+    if (themeSetting == "system") {
+        const userPref = window.matchMedia;
+        if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+            return "dark";
+        } else {
+            return "light";
+        }
+    } else {
+        return themeSetting;
+    }
+};
+
+let initTheme = () => {
+    let themeSetting = getThemeSetting();
+    setThemeSetting(themeSetting);
+
+    // add event listener to the theme toggle button
+    document.addEventListener("DOMContentLoaded", function () {
+        const theme_toggle = document.getElementById("change-theme-button");
+        theme_toggle.addEventListener("click", function () {
+            toggleThemeSetting();
+        });
+    });
+
+    // add event listener to the system theme preference change
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
+        applyTheme();
+    });
+};
+
+initTheme();
