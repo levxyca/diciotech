bfb62bfe379ee8479c78b05ce6b8ead83d6dec92
George Araújo <george.gcac@gmail.com>
2024-11-14 16:34:53 -0300

    Removed json files from run_on_code_changed.sh
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- run_on_code_changed.sh
+++ run_on_code_changed.sh
@@ -2,8 +2,8 @@
 FILES=$(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g')
 [ -z "$FILES" ] && exit 0
 
-# Prettify all selected files
-echo "$FILES" | xargs npx prettier --ignore-unknown --write
+# Prettify all selected files unless they are json files
+echo "$FILES" | grep -v '\.json$' | xargs npx prettier --ignore-unknown --write
 
 # Add back the modified/prettified files to staging
 echo "$FILES" | xargs git add
